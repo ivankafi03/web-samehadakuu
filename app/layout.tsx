8aefb3b9a1ac47b5ef301c27e547f7d906703b6e
Anime Admin <anime@example.com>
2026-05-07 16:33:03 +0700

    feat: add Histats invisible tracker (ID: 5025180)

--- app/layout.tsx
+++ app/layout.tsx
@@ -89,6 +89,19 @@ export default async function RootLayout({
             strategy="lazyOnload"
           />
         )}
+        {/* Histats.com invisible tracker - semua pengunjung */}
+        <Script id="histats-tracker" strategy="lazyOnload">{`
+          var _Hasync = _Hasync || [];
+          _Hasync.push(['Histats.start', '1,5025180,4,0,0,0,00010000']);
+          _Hasync.push(['Histats.fasi', '1']);
+          _Hasync.push(['Histats.track_hits', '']);
+          (function() {
+            var hs = document.createElement('script');
+            hs.type = 'text/javascript'; hs.async = true;
+            hs.src = '//s10.histats.com/js15_as.js';
+            (document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(hs);
+          })();
+        `}</Script>
       </body>
     </html>
   );
